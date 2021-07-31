a76ef327d641bf131d63b88cd5e4d83dbcf91a39
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-07-31 12:48:53 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :furimas
+  has_many :purchases
+
+  validates :nickname, presence: true 
+
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message:'全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+  
+  with_opitions presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: '全角カタカナのみで入力して下さい'} do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+
+  validates :birthday, presence: true
+       
 end
