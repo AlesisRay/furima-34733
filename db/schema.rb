ca39b8684ce968043aed5636592c90b648c363f2
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-08-12 13:58:32 +0900

    Userモデル単体テスト

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_234121) do
+ActiveRecord::Schema.define(version: 2021_07_24_104744) do
+
+  create_table "furimas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
