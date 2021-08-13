cd309e6d5fbd76fc5ed6f31ac5e176d717b99f16
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-08-13 14:01:43 +0900

    Userモデル修正rev1

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     password              { '1030kt' }
     password_confirmation { password }
     last_name             { '拓也' }
-    first_name            { '川本' }
+    first_name            { '佐々木' }
     last_name_kana        { 'タクヤ' }
-    first_name_kana       { 'カワモト' }
+    first_name_kana       { 'カワモトー' }
     birthday              { '1991-05-16' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -68,6 +68,14 @@ RSpec.describe User, type: :model do
       expect(another_user.errors.full_messages).to include 'Email has already been taken'
     end
 
+     # email ＠必須
+
+     it 'emailは、@を含む必要があること' do
+      @user.email = 'takuya.com'
+      @user.valid?
+      expect(@user.errors.full_messages).to include "Email is invalid"
+    end
+
     # password 6文字
 
     it 'passwordが6文字以上であれば登録できる' do
@@ -89,16 +97,26 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include 'Password is invalid'
     end
+    it 'passwordが半角英数のみでは登録できない' do
+      @user.password = 'aaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include 'Password is invalid'
+    end
+    it 'passwordに全角英数があれば登録できない' do
+      @user.password = '12345ｋ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include 'Password is invalid'
+    end
 
     # 全角文字制限 名前
 
     it 'first_nameが全角文字でなければ登録できない' do
-      @user.first_name = 'ｶﾜﾓﾄ'
+      @user.first_name = 'ｶﾜﾓﾄー'
       @user.valid?
       expect(@user.errors.full_messages).to include 'First name 全角文字を使用してください'
     end
     it 'last_nameが全角文字でなければ登録できない' do
-      @user.last_name = 'ﾀｸﾔ'
+      @user.last_name = 'ﾀｸﾔ々'
       @user.valid?
       expect(@user.errors.full_messages).to include 'Last name 全角文字を使用してください'
     end
