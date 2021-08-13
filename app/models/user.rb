f8d88f18630b647ca4d2f126c647b89408c545a5
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-08-13 14:01:25 +0900

    バリデーション修正rev1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :furimas
-  has_many :purchases
-
   validates :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/ }
-  validates :nickname, presence: true
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
 
-  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: '全角文字を使用してください' } do
-    validates :last_name
-    validates :first_name
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: '全角カタカナのみで入力して下さい' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
   end
-
-  with_options presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: '全角カタカナのみで入力して下さい' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-
-  validates :birthday, presence: true
+  
 end
