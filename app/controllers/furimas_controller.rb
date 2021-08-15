6f6867acb044e3a9ef811b6f189f2cdb50577480
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-08-15 23:24:58 +0900

    ログイン時の出品ページ遷移

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,10 +1,14 @@
 class FurimasController < ApplicationController
-  
+  before_action :authenticate_user!, only: [:new]
+
   def index
   end
 
   def new
     @furima = Furima.new
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 
 end
