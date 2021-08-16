fc185a8ef0f8eafb479ed5bbd4e2e11c4ddfe91c
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-08-16 17:05:50 +0900

    出品機能（ルーティング・コントローラーの編集）

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -11,6 +11,15 @@ class FurimasController < ApplicationController
     end
   end
 
+  def create
+    @furima = Furima.new(furima_params)
+    if @furima.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
    def furima_params
     params.require(:furima).permit(:image, :title, :price, :text, :category_id, :item_status_id, :shipping_charges_id, :shipping_date_id, :prefecture_id).merge(user_id: current_user.id)
