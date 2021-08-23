94adc32df6e0a0b66d08448384401558b86abe63
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-08-23 16:16:53 +0900

    金額計算(JavaScript)

---
+++ app/javascript/new.js
@@ -0,0 +1,13 @@
+
+window.addEventListener('load', function(){
+  const price = document.getElementById("item-price");
+  const taxPrice = document.getElementById('add-tax-price');
+  const profit = document.getElementById('profit');
+
+  price.addEventListener('input', function(){
+    const tax = 0.10;
+    const value = price.value;
+    taxPrice.innerHTML = value*tax;
+    profit.innerHTML = value*(1-tax);
+  })
+})
