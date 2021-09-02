72938ba0b67d6bc19fb3b2a27f1a76308a3d6653
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-09-02 15:51:30 +0900

    購入機能コントローラー実装

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,26 @@
+class PurchasesController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @furima = Furima.find(params[:furima_id])
+    @purchase_customer = PurchaseCustomer.new
+  end
+
+  def create
+    @furima = Furima.find(params[:furima_id])
+    @purchase_customer = PurchaseCustomer.new(purchase_params)
+    if @purchase_customer.valid?
+      @purchase_customer.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_customer).permit(:postal_code, :prefecture_id, :municipality, :address, :phone_number, :building).merge(user_id: current_user.id, furima_id: @furima.id)
+  end
+
+end
