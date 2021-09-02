60e2c71e196d6e58af4edc5ff36b18a58c6ff187
AlesisRay <kawamoto.takuya.fukui@gmail.com>
2021-09-02 15:52:29 +0900

    購入機能モデル修正rev1

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,11 +2,4 @@ class Customer < ApplicationRecord
   belongs_to :purchase
   belongs_to :prefecture
 
-  with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :municipality
-    validates :address
-    validates :phone_number, format: { with: /\A\d{11}\z/ }
-  end
 end
---
+++ app/models/purchase_customer.rb
@@ -0,0 +1,20 @@
+class PurchaseCustomer
+  include ActiveModel::Model
+  attr_accessor :user_id, :furima_id, :postal_code, :prefecture_id, :municipality, :address, :building, :phone_number, :purchase
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :municipality
+    validates :address
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
+    validates :user_id
+    validates :furima_id
+  end
+
+  def save
+    purchase = Purchase.create(furima_id: furima_id, user_id: user_id)
+
+    Customer.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, phone_number: phone_number, building: building, purchase_id: purchase.id)
+  end
+end
